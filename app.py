fd5d25e9bf08beca82412c71734e377caa493c13
FastDevelopmentP <pete5fast@gmail.com>
2025-09-14 17:34:44 -0400

    final

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 class Product:
-    def __init__(self, name, price, product_id):
+    def __init__(self, name, price, sku):
         self.name = name
         self.price = price
-        self.product_id = product_id
+        self.sku = sku
 
     def __str__(self):
-        return f"{self.name} (SKU: {self.sku}) {self.price:.2f}"
+        return f"{self.name} (SKU: {self.sku}) - ${self.price:.2f}"
 
 
 class ShoppingCart:
-    pass
+    #define the constructor
+    def __init__(self):
+        self.items = []
+
+    #define the __str__ method
+    def __str__(self):
+        return f"Shopping Cart with {len(self.items)} items."
+    #define the add_items method
+    def add_items(self, product, quantity=1):
+        self.items.append({"product": product, "quantity": quantity})
+
+    def get_total(self):
+        #initialize total to 0
+        total = 0
+
+
+        #loop through the items in the cart and sum up the values
+        for item in self.items:
+            total += item["product"].price * item["quantity"]
+
+
+
+        #return the total
+        return total
+
+    def display_cart(self):
+        #interate through the items in the cart and print them out
+        for item in self.items:
+           print(f"{item['product']} - Quantity: {item['quantity']}")
+
+        print(f"Total: ${self.get_total():.2f}")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from app import Product
+from app import Product, ShoppingCart
 laptop = Product ("Macbook Pro", 1299.98, 123456)
 headphones = Product ("Solo 4", 149.99, 983654) 
 
 
 print (laptop)
 print (headphones)
+cart = ShoppingCart()
+
+cart.add_items(laptop)
+cart.add_items(headphones, 3)   
+
+print (cart.items)
+print (cart)
+
+cart.display_cart()
