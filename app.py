bb491e4d8ccf4a095a709046553e533cab096d4c
FastDevelopmentP <pete5fast@gmail.com>
2025-09-13 23:55:36 -0400

    first save

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 class Product:
-    pass
+    def __init__(self, name, price, product_id):
+        self.name = name
+        self.price = price
+        self.product_id = product_id
+
+    def __str__(self):
+        return f"{self.name} (SKU: {self.sku}) {self.price:.2f}"
+
 
 class ShoppingCart:
     pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from app import Product
+laptop = Product ("Macbook Pro", 1299.98, 123456)
+headphones = Product ("Solo 4", 149.99, 983654) 
+
+
+print (laptop)
+print (headphones)
